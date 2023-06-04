d90563cafce2e22e4dbe9253837f9f8c88571d77
Rik Mentink <rik.mentink@icloud.com>
2023-06-04 18:01:41 +0200

    Na lang strugglen een werkende endpoint gemaakt.

--- src/main/webapp/assets/js/script.js
+++ src/main/webapp/assets/js/script.js
@@ -1,90 +1,117 @@
 const API_URL = 'http://localhost:8080/studybuddy-0.1.0/api';
+const URL_PREFIX = '/studybuddy-0.1.0';
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Stores a user in the storage to demonstrate, if none is set.
-    setCurrentUser(1);
-
-    // Load all projects
-    switch (this.location.pathname) {
-        case '/projects.html':
-            fetchProjects();
-            break;
-    }
-});
-
+// Saves the current user id in the client's session storage.
 function setCurrentUser(userId) {
     if (!getCurrentUser()) {
         sessionStorage.setItem('userId', userId.toString());
     }
 }
 
+// Reads the current user id from the client's session storage.
 function getCurrentUser() {
     return sessionStorage.getItem('userId');
 }
 
-// Fetches project data and places it in the projects container.
-async function fetchProjects() {
-    const res = await fetch(`${API_URL}/projects/${getCurrentUser()}`);
-    const data = await res.json();
+// Translates data of a given form into clean json.
+function getJsonData(form) {
+    let formData = new FormData(form);
+    let jsonData = {};
 
-    if (res.ok) {
-        if (data.length > 0) {
-            for (let project in projects) {
-                const projectCard = document.importNode(projectCardTemplate.textContent, true);
-                
-                // projectCard.querySelector("#image").src = 
-                projectCard.querySelector("#title").textContent = project.name;
-                projectCard.querySelector("#description").textContent = project.description;
-                projectCard.querySelector("#date").textContent = project.startDate + " tot " + project.endDate;
-                // projectCard.querySelector("#url").href = 
-        
-                document.querySelector('#projectsContainer').appendChild(projectBlock);
-            }
-        } else {
-            const message = 'Je hebt nog geen projecten. Maak er hieronder een aan!';
-            document.querySelector('#projectsContainer').textContent = message;
-        }
-    } else {
-        console.error('API error: ' + data.error);
-
-        const message = 'Er is een fout opgetreden, probeer het eens opnieuw.';
-        document.querySelector('#projectsContainer').textContent = message;
-    }
+    formData.forEach((value, key) => (jsonData[key] = value));
+    return jsonData;
 }
 
-// Adds a new project linked to the current user
-async function addProject(jsonBody) {
-    const res = await fetch(`${API_URL}/projects`, { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: { 
-            "studentId": parseInt(getCurrentUser()),
-            "name": name, 
-            "description": description, 
-            "startDate": startDate, 
-            "endDate": endDate 
-        }
+// Initializes app on DOM load.
+if (document.readyState !== 'loading') {
+    initializeApp();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        initializeApp();
     });
-    const data = await res.json();
+}
 
-    if (res.ok) {
-        console.log(data);
-    } else {
-        console.error('API error: ' + data.error);
+// Lazy loads certain data on different pages.
+function initializeApp() {
+    // Stores a user in the storage to demonstrate, if none is set.
+    setCurrentUser(1);
+ 
+    switch (this.location.pathname) {
+        case `${URL_PREFIX}/projects.html`:
+            getUserProjects();
+            break;
     }
+
+    console.log('INFO: App has been initialized.');
+}
+
+// Fetches project data and places it in the projects container.
+function getUserProjects() {
+    fetch(`${API_URL}/students/${getCurrentUser()}/projects`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            if (data.length > 0) {
+                const projectCardTemplate = document.querySelector('#projectCardTemplate');
+
+                data.forEach((project) => {
+                    console.log(project);
+                    const projectCard = document.importNode(
+                        projectCardTemplate.content,
+                        true
+                    );
+
+                    // projectCard.querySelector('#image').src =
+                    projectCard.querySelector('#title').textContent = project.name;
+                    projectCard.querySelector('#description').textContent =
+                        project.description;
+                    projectCard.querySelector('#date').textContent =
+                        project.startDate + ' tot ' + project.endDate;
+                    // projectCard.querySelector('#url').href =
+
+                    document
+                        .querySelector('#projectsContainer')
+                        .appendChild(projectCard);
+                });
+            } else {
+                const message =
+                    'Je hebt nog geen projecten. Maak er hieronder een aan!';
+                document.querySelector('#projectsContainer').textContent = message;
+            }
+        })
+        .catch((err) => {
+            document.querySelector('#projectsContainer').textContent =
+                'Something went wrong!';
+            console.error(err);
+        });
 }
 
 // Forms
-let newProjectForm = document.querySelector('#newProjectForm');
-newProjectForm.addEventListener('submit', function (e) {
-    let jsonBody = {};
+document.querySelector('#newProjectForm').addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    if (document.querySelector('input[name="name"').value == "") {
-        alert("Vul een naam voor je project in!");
-    } else {
-        new FormData(document.querySelector('#newProjectForm')).forEach((value, key) => jsonBody[key] = value);
-        addProject(jsonBody);
-    }
+    fetch(`${API_URL}/students/${getCurrentUser()}/projects`, {
+        method: 'POST',
+        body: getJsonData(e.target),
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((result) => {
+            console.log(result);
+            document.querySelector('#newProjectMessage').textContent =
+                'Project successfully added!';
+            e.target.reset();
+        })
+        .catch((err) => {
+            document.querySelector('#newProjectMessage').textContent =
+                'Something went wrong!';
+            console.error(err);
+        });
 });
