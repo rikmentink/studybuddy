f21e46ea0debcfac2620de8637e0c814e9e08bf4
Rik Mentink <rik.mentink@icloud.com>
2023-06-21 10:32:31 +0200

    Added add, update and delete functions for objectives.

--- src/main/java/me/rikmentink/studybuddy/model/Objective.java
+++ src/main/java/me/rikmentink/studybuddy/model/Objective.java
@@ -2,30 +2,36 @@ package me.rikmentink.studybuddy.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import me.rikmentink.studybuddy.handler.FileHandler;
+
 public class Objective {
     private int id;
     private String name;
     private String description;
+    private String note;
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime deadline;
 
-    public Objective(String name, String description, LocalDateTime deadline) {
+    public Objective(String name, String description, LocalDateTime deadline, String note) {
         this.name = name;
         this.description = description;
         this.deadline = deadline;
+        this.note = note;
     }
 
     public Objective(@JsonProperty("id") int id, 
                      @JsonProperty("name") String name, 
                      @JsonProperty("description") String description, 
-                     @JsonProperty("deadline") LocalDateTime deadline) {
-        this(name, description, deadline);
+                     @JsonProperty("deadline") LocalDateTime deadline,
+                     @JsonProperty("note") String note) {
+        this(name, description, deadline, note);
         this.id = id;
     }
 
@@ -41,12 +47,32 @@ public class Objective {
         return this.name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return this.description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public LocalDateTime getDeadline() {
         return this.deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getNote() {
+        return this.note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
     }
 
     /**
@@ -76,7 +102,83 @@ public class Objective {
                 .orElse(null);
     }
 
-    // TODO: Create addObjective(), updateObjective() and deleteObjective() methods.
+    /**
+     * Adds a new objective to the objectives of a specific project.
+     * 
+     * @param projectId The ID of the project to whom the objective belongs.
+     * @param project   The Objective object to add.
+     * @return True if the objective was successfully added, false otherwise.
+     */
+    public static boolean addObjective(int projectId, Objective objective) {
+        List<Student> students = Student.getAllStudents();
+        Optional<Student> optionalStudent = students.stream()
+                .filter(student -> student.getProjects().stream()
+                        .anyMatch(project -> project.getId() == projectId))
+                .findFirst();
+
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.get();
+            Optional<Project> optionalProject = student.getProjects().stream()
+                    .filter(project -> project.getId() == projectId)
+                    .findFirst();
+
+            if (optionalProject.isPresent()) {
+                Project project = optionalProject.get();
+                List<Objective> objectives = project.getObjectives();
+                objectives.add(objective);
+
+                return FileHandler.writeData(students);
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * TODO: Document function.
+     * 
+     * @param objectiveId
+     * @param updatedObjective
+     * @return
+     */
+    public static boolean updateObjective(int objectiveId, Objective updatedObjective) {
+        List<Student> students = Student.getAllStudents();
+        
+        students.stream()
+            .flatMap(student -> student.getProjects().stream())
+            .filter(project -> project.getObjectives().stream()
+                    .anyMatch(objective -> objective.getId() == objectiveId))
+            .findFirst()
+            .ifPresent(project -> {
+                List<Objective> objectives = project.getObjectives();
+                objectives.stream()
+                    .filter(objective -> objective.getId() == objectiveId)
+                    .findFirst()
+                    .ifPresent(objective -> {
+                        objective.setName(updatedObjective.getName());
+                        objective.setDescription(updatedObjective.getDescription());
+                        objective.setDeadline(updatedObjective.getDeadline());
+                        objective.setNote(updatedObjective.getNote());
+                    });
+            });
+
+        return FileHandler.writeData(students);
+    } 
+
+    /**
+     * TODO: Document function.
+     * 
+     * @param objectiveId
+     * @return
+     */
+    public static boolean deleteObjective(int objectiveId) {
+        List<Student> students = Student.getAllStudents();
+        
+        students.forEach(student -> student.getProjects().forEach(project ->
+                project.getObjectives().removeIf(objective -> objective.getId() == objectiveId)));
+
+        return FileHandler.writeData(students);
+    } 
 
     public static int generateNewObjectiveId() {
         List<Objective> objectiveList = getAllObjectives();
--- src/main/java/me/rikmentink/studybuddy/model/Task.java
+++ src/main/java/me/rikmentink/studybuddy/model/Task.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 public class Task extends Objective {
     private boolean completed;
 
-    public Task(int id, String name, String description, LocalDateTime deadline, boolean completed) {
-        super(id, name, description, deadline);
+    public Task(int id, String name, String description, LocalDateTime deadline, String note, boolean completed) {
+        super(id, name, description, deadline, note);
         this.completed = completed;
     }
 
     public boolean getCompleted() {
         return this.completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
